fe754635957dfaa923401b2a6093c7ecf6a9b85d
kapiLveraM <117080546+kapiLveraM@users.noreply.github.com>
2023-11-27 14:15:12 +0530

    code inpruve

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Hero from "./components/Hero";
